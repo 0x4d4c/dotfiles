e7f360d56a795e35fb846710f80a8fdecd36f769
Martin Lambertz <martin@0x4d4c.xyz>
2019-03-19 17:01:18 +0100

    Use short variable for XDG_CONFIG_HOME

--- install-dotfiles.sh
+++ install-dotfiles.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -eu
 
 dots="$(dirname $(readlink -e $0))"
+cfg="${HOME}/.config"
 
 function _ln() {
   ln -snf "${dots}/$1" "$2"
@@ -10,10 +11,10 @@ function _ln() {
 mkdir -p ~/.config
 mkdir -p ~/.local/bin
 
-_ln dircolors ~/.config/dircolors
+_ln dircolors ${cfg}/dircolors
 _ln git/config ~/.gitconfig
-_ln i3 ~/.config/i3
-_ln rofi ~/.config/rofi
+_ln i3 ${cfg}/i3
+_ln rofi ${cfg}/rofi
 _ln xinitrc ~/.xinitrc
 _ln zprofile ~/.zprofile
 
